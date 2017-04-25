e74f06c40489308c97564e1cc5c20be0344b5803
StephenFenner1 <stephen.fenner@hotmail.co.uk>
2017-04-25 12:50:01 +0100

    Portfolio

--- src/softwareengineering/Portfolio.java
+++ src/softwareengineering/Portfolio.java
@@ -1,43 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softwareengineering;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- * @author stf23
+ *  Class for the portfolio of the client. Takes the client and creates a portfolio for that client.
+ *  The constructor creates the portfolio, the other methods allow to get information on the client
+ *  and the contents of the portfolio.
  */
 public class Portfolio {
     
     private Map stockOwned; //Map(companyName, stockValue)
     private Client client; //the client    
     
-    public Portfolio(Client client, String[] companyName, Integer[] stocks) {
-        this.client = client;
-        List<String> companyNames = new ArrayList<String>(Arrays.asList(companyName)); //turn companyName into list
-        List<Integer> stockPrice = new ArrayList<Integer>(Arrays.asList(stocks)); ////turn stocks into list
-        
-        //both lists have to be the same size
-        if(companyNames.size() == stockPrice.size()) {
-            //concat lists into map with key = companyName, value = stockValue;
-            for(int i = 0; i < companyNames.size(); i++) {
-                stockOwned.put(companyNames.get(i), stockPrice.get(i));
+    /*
+    * Constructor method for the Portfolio class. Creates a new portfolio for the 
+    * specified Client. Initialises and fills a HashMap with the contents
+    * of two arrays (read from the initialisation data), also intialises the client
+    * that the portfolio belongs too.
+    *
+    * @param client         The client who owns the portfolio
+    * @param companyName    Array that contains the company names respective to the client from the initialisation data
+    * @param stocks         Array that contains the stock prices respective to the client from the initialisation data
+    */
+    public Portfolio(Client client, String[] companyName, int[] stocks) {
+        this.client = client;        
+        stockOwned = new HashMap<String, Integer>();
+        //both arrays have to be the same size
+        if(companyName.length == stocks.length) {
+            //concat arrays into map with key = companyName, value = stockValue;
+            for(int i= 0; i < companyName.length; i++){
+                stockOwned.put(companyName[i], stocks[i]);
             }
         }
     }    
 
+    /*
+    * Getter method for the client that the portfolio belongs to
+    *
+    * @return The client object.
+    */
     public Client getClient() {
         return client;
     }
-
+    
+    /*
+    * Getter method for the stockOwned Map
+    *
+    * @return The stockOwned Map.
+    */
     public Map getStockOwned() {
         return stockOwned;
-    }
+    } 
+    
+    /*
+    * Method to print the contents of the portfolio in the format: ("Company name" : "Stock price")
+    */
+    public void printPortfolio() {
+        for(Object company : stockOwned.keySet()) {
+            String key = company.toString();
+            String value = stockOwned.get(company).toString();
+            System.out.println(key + " : " + value);
+        }
+    } 
 }

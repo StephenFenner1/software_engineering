916bb14331e9d4e3bf5770a06d088d9ee7950973
jc6281997 <doom30@hotmail.co.uk>
2017-04-27 14:53:40 +0100

    Update Mood.java

--- src/softwareengineering/Mood.java
+++ src/softwareengineering/Mood.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softwareengineering;
 
 /**
- *
+ * An enumerator initialisation used to define the traders mood.
+ * 
  * @author Josh Hasan
- * An enum initialisation used to define the traders mood.
  */
- public enum Mood {
+public enum Mood {
     Balanced, AggressiveSeller, AggressivePurchaser;
 }

28b47efbefb25a364e0499c0eda5e359906691c3
Josh590 <josh.hasan590@gmail.com>
2017-04-27 12:15:18 +0100

    Create Mood.java

---
+++ src/softwareengineering/Mood.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareengineering;
+
+/**
+ *
+ * @author Josh Hasan
+ * An enum initialisation used to define the traders mood.
+ */
+ public enum Mood {
+    Balanced, AggressiveSeller, AggressivePurchaser;
+}

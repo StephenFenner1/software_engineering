9f3c2bc81604a6c0d2c086f1b4f498499e5a10eb
StephenFenner1 <stephen.fenner@hotmail.co.uk>
2017-04-27 17:39:14 +0100

    Change arrays to arraylists and updated docs

--- src/softwareengineering/Portfolio.java
+++ src/softwareengineering/Portfolio.java
@@ -16,22 +16,23 @@ public class Portfolio {
     /*
     * Constructor method for the Portfolio class. Creates a new portfolio for the 
     * specified Client. Initialises and fills a HashMap with the contents
-    * of two arrays (read from the initialisation data), also intialises the client
-    * that the portfolio belongs too.
+    * of two ArrayLists (read from the initialisation data), also adds the client
+    * and trader that the portfolio belongs to and handles respectively.
     *
     * @param client         The client who owns the portfolio.
-    * @param companyName    Array that contains the company names respective to the client from the initialisation data.
-    * @param stocks         Array that contains the stock prices respective to the client from the initialisation data.
+    * @param companyName    ArrayList that contains the company names respective to the client from the initialisation data.
+    * @param stocks         ArrayList that contains the stock prices respective to the client from the initialisation data.
+    * @param trader         The trader handling the portfolio
     */
-    public Portfolio(Client client, String[] companyName, int[] stocks, Trader trader) {
+    public Portfolio(Client client, ArrayList<String> companyName, ArrayList<Integer> stocks, Trader trader) {
         this.client = client;     
         this.trader = trader;
         stockOwned = new HashMap<String, Integer>();
         //both arrays have to be the same size
-        if(companyName.length == stocks.length) {
+        if(companyName.size() == stocks.size()) {
             //concat lists into map with key = companyName, value = stockValue;
-            for(int i= 0; i < companyName.length; i++){
-                stockOwned.put(companyName[i], stocks[i]);
+            for(int i= 0; i < companyName.size(); i++){
+                stockOwned.put(companyName.get(i), stocks.get(i));
             }
         }
     } 

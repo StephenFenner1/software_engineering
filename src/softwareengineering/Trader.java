cfdd25a1ce78464a8ad449d9310ac52cdf449ed8
jc6281997 <doom30@hotmail.co.uk>
2017-04-27 14:41:17 +0100

    Update Trader.java

--- src/softwareengineering/Trader.java
+++ src/softwareengineering/Trader.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softwareengineering;
 
 import java.util.List;
 
 /**
  *
- * @author Josh Hasan
+ * @author Josh Hasan, Jamie Critcher
  */
 public abstract class Trader {
     
@@ -17,7 +12,8 @@ public abstract class Trader {
     
     public abstract int requestTrade(Company company, Portfolio portfolio);
     
-    public void makeTrade(Integer stocks, Company company) {
-        
+    public void makeTrade(Integer stocks, Company company, Portfolio portfolio) {
+        int var = (int)portfolio.getStockOwned().get(company);
+        portfolio.getStockOwned().replace(company, var + stocks);
     }
 }

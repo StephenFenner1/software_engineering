c4e74ec6c4557be63aed1ab374ef866404102e61
jc6281997 <doom30@hotmail.co.uk>
2017-04-27 14:53:17 +0100

    Create StockType.java

---
+++ src/softwareengineering/StockType.java
@@ -0,0 +1,10 @@
+package softwareengineering;
+
+/**
+ * An enumerator initialisation used to define the traders mood.
+ *
+ * @author Jamie Critcher
+ */
+public enum StockType {
+    Food, Hard, Property, Hitech;
+}

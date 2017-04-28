cb1ab2e8a4281f360db0c09675293490000fba19
jc6281997 <doom30@hotmail.co.uk>
2017-04-28 15:54:41 +0100

    Update Trader.java

--- src/softwareengineering/Trader.java
+++ src/softwareengineering/Trader.java
@@ -9,7 +9,23 @@ public abstract class Trader {
     public abstract int requestTrade(Company company, Portfolio portfolio);
     
     public void makeTrade(Integer stocks, Company company, Portfolio portfolio) {
+        // Update Client's cash holding.
+        portfolio.getClient().setCashHolding(portfolio.getClient().getCashHolding() - (stocks * company.getStockValue()));
+        
+        // Update number of stocks held.
         int var = (int)portfolio.getStockOwned().get(company);
         portfolio.getStockOwned().replace(company, var + stocks);
+    }
+    
+    protected boolean compareRisk(Risk clientRisk, Risk companyRisk) {
+        if (clientRisk == Risk.High) {
+            return true;
+        } else if (clientRisk == Risk.Moderate && (companyRisk == Risk.Moderate || companyRisk == Risk.Low)) {
+            return true;
+        } else if (clientRisk == Risk.Low && companyRisk == Risk.Low) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }

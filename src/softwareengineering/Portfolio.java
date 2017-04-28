f966f6c6b3236b280289680145ae3842ef540403
jc6281997 <doom30@hotmail.co.uk>
2017-04-28 15:52:59 +0100

    Update Portfolio.java

--- src/softwareengineering/Portfolio.java
+++ src/softwareengineering/Portfolio.java
@@ -2,7 +2,7 @@ package softwareengineering;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *  Class for the portfolio of the client. Takes the client and creates a portfolio for that client.
@@ -11,46 +11,83 @@ import java.util.ArrayList;
  */
 public class Portfolio {
     
-    private Map stockOwned; //Map(companyName, stockValue)
-    private Client client; //the client    
-    private Trader trader; //the trader
-    /*
-    * Constructor method for the Portfolio class. Creates a new portfolio for the 
-    * specified Client. Initialises and fills a HashMap with the contents
-    * of two ArrayLists (read from the initialisation data), also adds the client
-    * and trader that the portfolio belongs to and handles respectively.
-    *
-    * @param client         The client who owns the portfolio.
-    * @param companyName    ArrayList that contains the company names respective to the client from the initialisation data.
-    * @param stocks         ArrayList that contains the stock prices respective to the client from the initialisation data.
-    * @param trader         The trader handling the portfolio
-    */
-    public Portfolio(Client client, ArrayList<String> companyName, ArrayList<Integer> stocks, Trader trader) {
+    private Map stockOwned;         // Map(companyName, stockValue).
+    private final Client client;    // The client.
+    private final  Trader trader;   // The trader.
+    private Risk risk;              // Risk decided by the client.
+    
+    /**
+     * Constructor method for the Portfolio class. Creates a new portfolio for the 
+     * specified Client. Initialises and fills a HashMap with the contents
+     * of two arrays (read from the initialisation data), also initialises the client
+     * that the portfolio belongs too.
+     *
+     * @param client         The client who owns the portfolio.
+     * @param companyName    Array that contains the company names respective to the client from the initialisation data.
+     * @param stocks         Array that contains the stock prices respective to the client from the initialisation data.
+     */
+    public Portfolio(Client client, String[] companyName, int[] stocks, Trader trader) {
         this.client = client;     
         this.trader = trader;
         stockOwned = new HashMap<String, Integer>();
         //both arrays have to be the same size
-        if(companyName.size() == stocks.size()) {
+        if(companyName.length == stocks.length) {
             //concat lists into map with key = companyName, value = stockValue;
-            for(int i= 0; i < companyName.size(); i++){
-                stockOwned.put(companyName.get(i), stocks.get(i));
+            for(int i= 0; i < companyName.length; i++){
+                stockOwned.put(companyName[i], stocks[i]);
             }
         }
+        // Randomly assign a risk to the Portfolio.
+        Random rand = new Random();
+        float var = (5 * rand.nextFloat());
+        if (var < 1) {
+            risk = Risk.Low;
+        } else if ( var < 4) {
+            risk = Risk.Moderate;
+        } else {
+            risk = Risk.High;
+        }
     } 
-    /*
-    * Getter method for the trader that the portfolio belongs to
-    *
-    * @return The trader object.
-    */
+    /**
+     * Getter method for the trader that the portfolio belongs to
+     *
+     * @return The trader object.
+     */
     public Trader getTrader() {
         return trader;
     }
+    
+    public void updateClientRisk() {
+        // Randomly assign a risk to the Portfolio.
+        Random rand = new Random();
+        float var = (5 * rand.nextFloat());
+        if (var < 1) {
+            risk = Risk.Low;
+        } else if ( var < 4) {
+            risk = Risk.Moderate;
+        } else {
+            risk = Risk.High;
+        }
+    }
+    
+    /**
+     * Public method to update the current value of a portfolios client.
+     */
+    public void updatePortfolio() {
+        // Get the total value of the clients owned stocks.
+        int var = 0;
+        for (Object companyName : stockOwned.keySet()) {
+            var += getPrice((String)companyName);
+        }
+        // Set the clients current value to their stock value + cash holding.
+        client.setCurrentValue(client.getCashHolding() + var);
+    }
 
-    /*
-    * Getter method for the client that the portfolio belongs to
-    *
-    * @return The client object.
-    */
+    /**
+     * Getter method for the client that the portfolio belongs to.
+     *
+     * @return The client object.
+     */
     public Client getClient() {
         return client;
     }
@@ -64,19 +101,24 @@ public class Portfolio {
         return stockOwned;
     }
     
-    /*
-    * Gets the price of the stock of a company given in the clients portfolio
-    *
-    * @param  name   The company name.
-    * @return        The price of the companies stock.      
-    */
+    /**
+     * Gets the price of the stock of a company given in the clients portfolio
+     *
+     * @param  name   The company name.
+     * @return        The price of the companies stock.      
+     */
     public int getPrice(String name) {
         return (int) stockOwned.get(name);
     }
     
-    /*
-    * Method to print the contents of the portfolio in the format: ("Company name" : "Stock price")
-    */
+    public Risk getRisk() {
+        return risk;
+    }
+    
+    /**
+     * Method to print the contents of the portfolio in the format:
+     * ("Company name" : "Stock price").
+     */
     public void printPortfolio() {
         for(Object company : stockOwned.keySet()) {
             String key = company.toString();

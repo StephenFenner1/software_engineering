094119ee65ca94556fc446fe6cceead006c6932d
jc6281997 <doom30@hotmail.co.uk>
2017-04-28 15:53:41 +0100

    Update RandomTrader.java

--- src/softwareengineering/RandomTrader.java
+++ src/softwareengineering/RandomTrader.java
@@ -2,87 +2,122 @@ package softwareengineering;
 
 /**
  * Class to handle a random trader and it's AI functionality.
- * 
+ *
  * @author Josh Hasan, Jamie Critcher
  */
-
 import java.util.Random;
 
 public class RandomTrader extends Trader {
-    
+
     private Mood mood;  // The selling mood of the Random Trader - {Balanced | AggressivePurchaser | AggressiveSeller}
-    
+    private Mood moodOverride;
+
     /**
-     *  Constructor method for the Random Trader.
-     *  Initialises all traders to have a balanced mood.
-     *  Stores all the portfolios relevant to this trader.
+     * Constructor method for the Random Trader. Initialises all traders to have
+     * a balanced mood. Stores all the portfolios relevant to this trader.
      */
     public RandomTrader() {
         this.mood = Mood.Balanced;
+        moodOverride = Mood.None;
+    }
+
+    public void setMoodOverride(Mood mood) {
+        moodOverride = mood;
+    }
+
+    public void removeMoodOverride() {
+        moodOverride = Mood.None;
     }
 
     /**
      * Public method to request a trade decision on a specific company and
      * portfolio.
-     * 
-     * @param company   The company that traders are trading stocks in.
+     *
+     * @param company The company that traders are trading stocks in.
      * @param portfolio The portfolio the traders is controlling.
-     * @return          The number of stocks to trade in the company.
+     * @return The number of stocks to trade in the company.
      */
     @Override
     public int requestTrade(Company company, Portfolio portfolio) {
-        Random rand = new Random();
-        float var = (3 * rand.nextFloat());
-        
-        if (var < 1) {
-            // Buy stocks.
-            var = rand.nextFloat();
-            switch (mood) {
-                case AggressiveSeller:
-                    return Math.round((var * (int)portfolio.getStockOwned().get(company)) / 200);
-                case Balanced:
-                    return Math.round((var * (int)portfolio.getStockOwned().get(company)) / 100);
-                case AggressivePurchaser:
-                    return Math.round((var * (int)portfolio.getStockOwned().get(company)) / 50);
-            }
-        } else if (var < 2) {
-            // Sell stocks.
-            var = rand.nextFloat();
-            switch (mood) {
-                case AggressiveSeller:
-                    return -(Math.round((var * (int)portfolio.getStockOwned().get(company)) / 50));
-                case Balanced:
-                    return -(Math.round((var * (int)portfolio.getStockOwned().get(company)) / 100));
-                case AggressivePurchaser:
-                    return -(Math.round((var * (int)portfolio.getStockOwned().get(company)) / 200));
+        // Check whether or not the client wants to cash out.
+        if (portfolio.getClient().isCashingOut()) {
+            // If cashing out, try to sell all stock.
+            return (int) portfolio.getStockOwned().get(company);
+        } else if(compareRisk(portfolio.getRisk(), company.getRisk())) {
+            Random rand = new Random();
+            float var = (3 * rand.nextFloat());
+
+            if (var < 1) {
+                // Buy stocks.
+                var = rand.nextFloat();
+                if (moodOverride == Mood.None) {
+                    selectBuyMood(portfolio, mood, var);
+                } else {
+                    selectBuyMood(portfolio, moodOverride, var);
+                }
+            } else if (var < 2) {
+                // Sell stocks.
+                var = rand.nextFloat();
+                if (moodOverride == Mood.None) {
+                    selectSellMood(portfolio, mood, var);
+                } else {
+                    selectSellMood(portfolio, moodOverride, var);
+                }
             }
         }
         // Do not trade.
         return 0;
     }
-    
+
+    private int selectBuyMood(Portfolio portfolio, Mood mood, float var) {
+        switch (mood) {
+            case AggressiveSeller:
+                return Math.round((var * (int) portfolio.getClient().getCashHolding()) / 200);
+            case Balanced:
+                return Math.round((var * (int) portfolio.getClient().getCashHolding()) / 100);
+            case AggressivePurchaser:
+                return Math.round((var * (int) portfolio.getClient().getCashHolding()) / 50);
+            default:
+                return 0;
+        }
+    }
+
+    private int selectSellMood(Portfolio portfolio, Mood mood, float var) {
+        switch (mood) {
+            case AggressiveSeller:
+                return Math.round((var * (int) portfolio.getClient().getCashHolding()) / 50);
+            case Balanced:
+                return Math.round((var * (int) portfolio.getClient().getCashHolding()) / 100);
+            case AggressivePurchaser:
+                return Math.round((var * (int) portfolio.getClient().getCashHolding()) / 200);
+            default:
+                return 0;
+        }
+    }
+
     /**
-     *  Getter method for the mood of the trader.
-     *  @return The mood of the trader.
+     * Getter method for the mood of the trader.
+     *
+     * @return The mood of the trader.
      */
     public Mood GetMood() {
         return mood;
     }
-    
+
     /**
-     *  public method to change the mood of the trader.
+     * public method to change the mood of the trader.
      */
     public void changeMood() {
         Random rand = new Random();
         float val = rand.nextFloat();
-        
-        switch(mood) {
+
+        switch (mood) {
             case AggressiveSeller:
                 if (val > 0.4) {
                     mood = Mood.Balanced;
                 }
                 break;
-                
+
             case Balanced:
                 if (val < 0.1) {
                     mood = Mood.AggressiveSeller;
@@ -90,7 +125,7 @@ public class RandomTrader extends Trader {
                     mood = Mood.AggressivePurchaser;
                 }
                 break;
-            
+
             case AggressivePurchaser:
                 if (val < 0.7) {
                     mood = Mood.Balanced;

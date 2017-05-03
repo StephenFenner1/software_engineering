9891b7c919aeaa6075aa2102b2c67773fcb2d0bf
jc6281997 <doom30@hotmail.co.uk>
2017-05-03 15:55:29 +0100

    Update Client.java

--- src/softwareengineering/Client.java
+++ src/softwareengineering/Client.java
@@ -3,7 +3,7 @@ package softwareengineering;
 /**
  * Storage class for client data. Stores the name, value and cash holding of a
  * client and whether or not the client is cashing out.
- * 
+ *
  * @author Jamie Critcher
  */
 public class Client {
@@ -18,10 +18,10 @@ public class Client {
     /**
      * Constructor for Client class. Initialises a single client data class
      * given basic information about a client.
-     * 
-     * @param clientName    The name of the client.
-     * @param cashHolding   The initials cash holding of the client.
-     * @param initialValue  The initial total value of the client.
+     *
+     * @param clientName The name of the client.
+     * @param cashHolding The initials cash holding of the client.
+     * @param initialValue The initial total value of the client.
      */
     public Client(String clientName, int cashHolding, int initialValue) {
         // Initialise clientName, cashHolding and initialValue to given values.
@@ -31,15 +31,20 @@ public class Client {
         // Initialise currentValue to match initialValue.
         this.currentValue = initialValue;
         // Set expectedValue to a 10% return.
-        this.expectedValue = (int)(initialValue * 1.1);
+        this.expectedValue = (int) (initialValue * 1.1);
         // Initialise cashingOut as false.
         this.cashingOut = false;
     }
 
+    @Override
+    public String toString() {
+        return clientName;
+    }
+
     /**
      * Setter method to set the current cash holding of a client to a new value.
-     * 
-     * @param cashHolding   New cash holding value.
+     *
+     * @param cashHolding New cash holding value.
      */
     public void setCashHolding(int cashHolding) {
         this.cashHolding = cashHolding;
@@ -47,8 +52,8 @@ public class Client {
 
     /**
      * Setter method to set the current total value of a client to a new value.
-     * 
-     * @param currentValue  New current total value.
+     *
+     * @param currentValue New current total value.
      */
     public void setCurrentValue(int currentValue) {
         this.currentValue = currentValue;
@@ -56,8 +61,8 @@ public class Client {
 
     /**
      * Setter method to set whether or not the client is cashing out.
-     * 
-     * @param cashingOut    Whether or not the client is cashing out.
+     *
+     * @param cashingOut Whether or not the client is cashing out.
      */
     public void setCashingOut(boolean cashingOut) {
         this.cashingOut = cashingOut;
@@ -65,7 +70,7 @@ public class Client {
 
     /**
      * Getter method to retrieve the stored name of the client.
-     * 
+     *
      * @return The name of the client.
      */
     public String getClientName() {
@@ -74,7 +79,7 @@ public class Client {
 
     /**
      * Getter method to retrieve the current cash holding of the client.
-     * 
+     *
      * @return The cash holding of the client.
      */
     public int getCashHolding() {
@@ -83,7 +88,7 @@ public class Client {
 
     /**
      * Getter method to retrieve the initial value of the client.
-     * 
+     *
      * @return The initial value of the client.
      */
     public int getInitialValue() {
@@ -92,7 +97,7 @@ public class Client {
 
     /**
      * Getter method to return the current value of the client.
-     * 
+     *
      * @return The current value of the client.
      */
     public int getCurrentValue() {
@@ -101,7 +106,7 @@ public class Client {
 
     /**
      * Getter method to retrieve the value the client expects in returns.
-     * 
+     *
      * @return The value the client expects in returns.
      */
     public int getExpectedValue() {
@@ -110,7 +115,7 @@ public class Client {
 
     /**
      * Getter method to retrieve whether or not the client wants to cash out.
-     * 
+     *
      * @return Whether or not the client wants to cash out.
      */
     public boolean isCashingOut() {

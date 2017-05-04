a1144cc66e8076c7c117db8ad46827688032b8be
StephenFenner1 <stephen.fenner@hotmail.co.uk>
2017-05-04 10:33:09 +0100

    Update MarketType.java

--- src/softwareengineering/MarketType.java
+++ src/softwareengineering/MarketType.java
@@ -1,15 +1,25 @@
 package softwareengineering;
 
 /**
- * An enumerator used to define a companies market type (Bull/Bear).
- * If a stock price falls, it is a Bear market.
- * If a stock price rises, it is a Bull market.
- * If a stock price remains unchanged, it maintains it's previous market type.
- * 
+ * An enumerator used to define a companies market type (Bull/Bear). 
  * All companies start with a randomly generated market type.
  * 
  * @author Jamie Critcher
  */
 public enum MarketType {
-    Bull, Bear, Stable;
+    
+    /**
+     * If a stock price falls, it is a Bear market
+     */
+    Bull,
+    
+    /**
+     * If a stock price rises, it is a Bull market.
+     */
+    Bear, 
+    
+    /**
+     * If a stock price remains unchanged, it maintains it's previous market type.
+     */
+    Stable;
 }

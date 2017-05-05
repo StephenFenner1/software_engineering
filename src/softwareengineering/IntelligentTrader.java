d7c6e4ab1a0495b6f2ea34bdceeb3a6708a04c55
jc6281997 <doom30@hotmail.co.uk>
2017-05-05 16:25:38 +0100

    Update IntelligentTrader.java

--- src/softwareengineering/IntelligentTrader.java
+++ src/softwareengineering/IntelligentTrader.java
@@ -2,15 +2,16 @@ package softwareengineering;
 
 /**
  * Class to handle an intelligent trader and it's AI functionality.
- * 
+ *
  * @author Josh Hasan, Jamie Critcher
  */
 public class IntelligentTrader extends Trader {
+
     private int id;
-    
+
     /**
-     *  Constructor method for the Intelligent Trader.
-     *  Initialises the trader a new ID.
+     * Constructor method for the Intelligent Trader. Initialises the trader a
+     * new ID.
      */
     public IntelligentTrader() {
         id = ID++;
@@ -19,37 +20,70 @@ public class IntelligentTrader extends Trader {
     /**
      * Public method to request a trade decision on a specific company and
      * portfolio.
-     * 
-     * @param company   The company that traders are trading stocks in.
+     *
+     * @param company The company that traders are trading stocks in.
      * @param portfolio The portfolio the traders is controlling.
-     * @return          The number of stocks to trade in the company.
+     * @return The number of stocks to trade in the company.
      */
     @Override
     public int requestTrade(Company company, Portfolio portfolio) {
+        // Check whether or not the client wants to cash out.
         if (portfolio.getClient().isCashingOut()) {
             // If cashing out, try to sell all stock.
-            return (int)portfolio.getStockOwned().get(company);
-        } else if(compareRisk(portfolio.getRisk(), company.getRisk())) {
+            if ((int) portfolio.getStockOwned().get(company) > 100) {
+                //If client owns more than 100 stock of this company, sell 100 at a time.
+                return -100;
+            } else {
+                //Otherwise, sell the remaining number of stocks.
+                return -(int) portfolio.getStockOwned().get(company);
+            }
+        } else if (compareRisk(portfolio.getRisk(), company.getRisk())) {
+            int amount = 0;
+            int stocks = 0;
             // Check market type of company.
             if (company.getMarketType() == MarketType.Bear) {
                 // Buy stocks.
-                return Math.round((int) portfolio.getAvailableMoney());
+                amount = Math.round((int) portfolio.getAvailableMoney());
+                stocks = (int) (amount / company.getStockValue());
+                return stocks;
             } else {
                 // Sell stocks.
-                return -Math.round(((int) portfolio.getAvailableAssets(company)));
+                amount = Math.round((int) portfolio.getAvailableAssets(company));
+                stocks = (int) (amount / company.getStockValue());
+                return -stocks;
             }
         }
         // Do not trade.
         return 0;
-    }  
-    
+    }
+
     /**
      * Getter method for the traders ID.
-     * 
-     * @return the traders ID.
+     *
+     * @return The traders ID.
      */
     @Override
     public int getID() {
-        return ID;
+        return id;
+    }
+
+    /**
+     * Getter method for the traders type.
+     * 
+     * @return "Intelligent".
+     */
+    @Override
+    public String getType() {
+        return "Intelligent";
+    }
+
+    /**
+     * Getter method for the traders mood.
+     * 
+     * @return "N/A".
+     */
+    @Override
+    public String getMood() {
+        return "N/A";
     }
 }

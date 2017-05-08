69dd681860324d2362d6f71179d4fc081b5ea200
Arbooshe <soccersukaserver@gmail.com>
2017-05-08 12:12:28 +0100

    Update Risk.java

--- src/softwareengineering/Risk.java
+++ src/softwareengineering/Risk.java
@@ -2,26 +2,23 @@ package softwareengineering;
 
 /**
  * An enumerator used to define a level of risk.
- * 
+ *
  * @author Group 26
  */
 public enum Risk {
-    
+
     /**
      * There is no risk taken.
      */
     None,
-    
     /**
      * There is a low level of risk taken.
      */
-    Low, 
-    
+    Low,
     /**
      * There is a moderate level of risk taken.
-     */    
+     */
     Moderate,
-    
     /**
      * There is a high level of risk taken.
      */

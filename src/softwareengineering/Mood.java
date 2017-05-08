a0930fae021be2bc52834864e2b2117ab44298d7
Arbooshe <soccersukaserver@gmail.com>
2017-05-08 12:11:44 +0100

    Update Mood.java

--- src/softwareengineering/Mood.java
+++ src/softwareengineering/Mood.java
@@ -2,26 +2,23 @@ package softwareengineering;
 
 /**
  * An enumerator initialisation used to define the traders mood.
- * 
+ *
  * @author Group 26
  */
 public enum Mood {
-    
+
     /**
      * The trader has no specified mood.
      */
     None,
-    
     /**
      * The trader will have a balanced mood. Will sell/buy equally.
      */
     Balanced,
-    
     /**
      * The trader is more likely to sell than buy.
      */
     AggressiveSeller,
-    
     /**
      * The trader is more likely to buy than sell.
      */
